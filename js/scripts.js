a240dc22cfdfd76c6fe947efe7553a1464cfe081
Caressa Archie <caressaarchie18@gmail.com>
2018-05-22 19:41:57 -0700

    added message to console

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+(function(){"use strict";
 var myName = "Caressa";
 
 console.log(myName);
+
+var message = "Hi Ryeker! I want to be able to use JavaScript in creating more dynamic websites or customzing website templates more efficiently...One of my favorite hobbies is to play tennis.";
+
+console.log(message);
+})();
