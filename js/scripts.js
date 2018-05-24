2989c9a1d6cad837da977e89326d74cd5e854cac
Caressa Archie <caressaarchie18@gmail.com>
2018-05-23 21:07:22 -0700

    added scripts and index

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ var myName = "Caressa Archie";
 let birthday = new Date("4.07.1994");
 const goal = "To understand JavaScript in order to be more marketable as a developer!";
 console.log(myName, birthday, goal);
+
+let favMovie = "The Matrix";
+let favFood = "Seafood Pasta";
+const year = new Date(3/31/1999);
+const ingredients = ["red sauce", "rigatoni pasta", "shrimp"]
+
+alert(favFood +" "+ ingredients);
+confirm(favMovie + " "+ year)
