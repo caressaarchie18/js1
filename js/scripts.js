060cbe833547d236c3d382cb9e4e2dddd8822161
Caressa Archie <caressaarchie18@gmail.com>
2018-05-23 20:53:48 -0700

    add index

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var myName = "Caressa Archie";
+let birthday = new Date("4.07.1994");
+const goal = "To understand JavaScript in order to be more marketable as a developer!";
+console.log(myName, birthday, goal);
