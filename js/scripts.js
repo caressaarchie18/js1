79d57b148747453a8b2a2ede8ee0cb779a75f9e1
Caressa Archie <36873549+caressaarchie18@users.noreply.github.com>
2018-05-28 18:22:34 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
-<<<<<< takehome-day2
-let userAnswerFN = prompt("What is your first name?");
-let userAnswerLN = prompt("What is your last name?");
-
-console.log(userAnswerFN);
-
-alert(userAnswerLN);
-
-let userBirthday = new Date('4.7.94');
-confirm(userBirthday);
-alert(userBirthday);
+let firstName = prompt ("What's your first name?", "John");
+console.log(firstName);
+let lastName = prompt ("What's your last name?", "Doe")
+alert(lastName);
+let birthday = prompt("What's your birthday?", "April 7 1994");
+confirm("Is " + birthday + " your birthday?");
